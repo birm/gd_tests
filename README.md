da6ea3cc11b0bffb670f48466b5316e661c10eac
Ryan Birmingham <birm@rbirm.us>
2021-02-15 14:56:23 -0500

    c sgd tests

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # sgd_tests
 Tests about SGD
+
+## Constants
+function : f(x) = (x-5)^2+7
+alpha  = 0.5
+delta: 0.01 (used as pseudo infinitesimal for gradient calculation)
+initial point: 1.0
+iterations: 10000000 ( 10 million )
+
+## Speed
+| Method | Description | Time |
+| ------ | ----------- | ---- |
+| c, gcc -O0 | c, not optimized | 0m0.334s |
+| c, gcc -O1 | c, `Moderate optimization; optimizes reasonably well but does not degrade compilation time significantly.` | 0m0.157s |
+| c, gcc -O2 | c, `Full optimization; generates highly optimized code and has the slowest compilation time.` | 0m0.002s |
+| c, gcc -O3 | c, `Full optimization as in -O2; also uses more aggressive automatic inlining of subprograms within a unit (Inlining of Subprograms) and attempts to vectorize loops.` | 0m0.001s |
+
+
+# Links
+GCC Optimization Flag quotes - https://gcc.gnu.org/onlinedocs/gnat_ugn/Optimization-Levels.html
---
+++ c/sgd.c
@@ -0,0 +1,51 @@
+// #include <stdio.h> // ONLY for my debugging
+
+// one dimensional SGD
+float SGD(float (*f)(float), float theta, int iters, float alpha, float delta){
+  // f is the function to optimize on
+  // theta is the inital theta point to try
+  // iters is the number of iterations to run
+  // alpha is the "learning rate"
+  // delta is the pseudo infinitesimal as used in gradient estimation
+  // NOTE there are substantial issues with this algorithm practicaly.
+  // DO NOT USE generally.
+
+  float lower;
+  float upper;
+  float grad;
+  float diff;
+  // go through iterations
+  for (int i=0; i<iters; i++){
+    // estimate gradient using pseudo infintesimal
+    upper = (1.0F + delta) * theta;
+    lower = (1.0F - delta) * theta;
+    diff = upper - lower;
+    upper = f(upper);
+    lower = f(lower);
+    grad = (upper - lower); // this line is problematic, but useful?
+    // printf("%f\n", grad);
+    // calculate new theta
+    theta -= alpha*grad;
+  }
+  return theta;
+}
+
+// minimal power function
+float pow(float x, int a){
+  float res = 1;
+  for (int i=0; i<a; i++){
+    res *= x;
+  }
+  return res;
+}
+
+// function, f(x) = (x-5)^2+7
+float f(float x){
+  return pow(x-5.0F, 2.0F)+7.0F;
+}
+
+int main(){
+  float theta = SGD(f, 1.0F, 10000000, 0.5F, 0.01F);
+   // printf("%f\n", theta);
+  return 0;
+}
